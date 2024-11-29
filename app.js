b5b3067771530aeb42ae5b9a18b5f6ae5a2f3fb5
Алексей Макаров <alexey.makarov@stecpoint.ru>
2024-11-29 15:35:34 +0800

    test push 5

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const publicApiKey = "xDPpWs7_uAJ30fRuNhlf5tTxpTj8HZMdyYx-7VMJzWk";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBPa1xmcpKA5gCaVSC0gelN9YPSRbpB7TY",
-    authDomain: "test-pushes-744ea.firebaseapp.com",
-    projectId: "test-pushes-744ea",
-    storageBucket: "test-pushes-744ea.firebasestorage.app",
-    messagingSenderId: "364415810958",
-    appId: "1:364415810958:web:74a40da5bd83183d32b8df",
-    measurementId: "G-ER37Y39E4E"
+    messagingSenderId: "364415810958"
 };
 
 firebase.initializeApp(firebaseConfig);
